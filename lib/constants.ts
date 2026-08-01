18c5e2e45df5f161a6406fb2a27fc01d7834f636
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-08-01 15:15:44 +0400

    fix: add www to trusted origins

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,9 +2,17 @@ export const APP_NAME = "BookSavat";
 
 export const DEVELOPMENT_PORT = 3456;
 
-export const PRODUCTION_URL = "https://booksavat.com";
+export const PRODUCTION_DOMAIN = "booksavat.com";
+
+export const PRODUCTION_URL = `https://${PRODUCTION_DOMAIN}`;
 export const DEVELOPMENT_URL = `http://localhost:${DEVELOPMENT_PORT}`;
 
 export const CURRENT_URL = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
 	: DEVELOPMENT_URL;
+
+export const TRUSTED_ORIGINS = [
+	PRODUCTION_URL,
+	DEVELOPMENT_URL,
+	`https://www.${PRODUCTION_DOMAIN}`,
+];
